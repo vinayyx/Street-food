6e0e0699f72e28e99a1b42b2c908140bdfc0624d
Vinay Kushwah <kushwahvinay95@gmail.com>
2025-07-27 22:50:34 +0530

    banckednupdate

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", withCredentials: true }));
+app.use(cors({ origin: "https://street-food-green.vercel.app", withCredentials: true }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
